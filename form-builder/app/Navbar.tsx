74392b531921941519863b3cb30c78ae2fa684f4
Ashitha Ebrahim <45201802+ASHITHAEBRAHIM@users.noreply.github.com>
2024-04-07 15:26:36 +0530

    Update Navbar.tsx

--- form-builder/app/Navbar.tsx
+++ form-builder/app/Navbar.tsx
@@ -1,18 +1,7 @@
-'use client'
 import { Button } from '@/components/ui/button'
-import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import { FaWpforms } from "react-icons/fa"
-
-
 const Navbar = () => {
-  const {status, data: session } = useSession();
-  const router = useRouter();
-  const handleLogin = async() => {
-   await signIn('/api/auth/signin'); 
-  };
-
   return (
 <nav className='flex justify-between items-center px-5 h-14 bg-black text-white'>
     <div className="flex items-center space-x-6 ">
@@ -23,8 +12,6 @@ const Navbar = () => {
         <Link href='/'>Features</Link>
         <Link href='/'>Pricing</Link>
         <Link href='/'>Contact</Link>
-        {status === 'authenticated' && <Button onClick={() => router.push('/form/list')} className='bg-blue-900 text-white'>Go to Forms</Button>}
-        {status === 'unauthenticated' && <Button onClick={handleLogin} className='bg-blue-900 text-white'>Login</Button>}
         <Button className='bg-blue-900 text-white'>Create Account</Button>
     </div>
 </nav>
