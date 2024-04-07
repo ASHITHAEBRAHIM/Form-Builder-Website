54553e273797cf76b360864c46173796ed244b31
Ashitha Ebrahim <45201802+ASHITHAEBRAHIM@users.noreply.github.com>
2024-04-07 19:44:41 +0530

    Update Signin.tsx

--- form-builder/app/Signin.tsx
+++ form-builder/app/Signin.tsx
@@ -3,16 +3,22 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { signIn, useSession } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
+import { useEffect } from 'react'
+
 const Signin = () => {
     const {status, data: session } = useSession();
     const router = useRouter();
     const handleLogin = async() => {
     await signIn('/api/auth/signin'); 
    };
+   useEffect(() => {
+    if (status === 'authenticated') {
+        router.push('/form/list');
+    }
+}, [status, router]);
   return (
     <div>
-         {status === 'authenticated' && <Button onClick={() => router.push('/form/list')} className='bg-blue-900 text-white'>Go to Forms</Button>}
-         {status === 'unauthenticated' && <Button onClick={handleLogin} className='bg-blue-900 text-white'>Login</Button>}
+         <Button onClick={handleLogin} className='bg-blue-900 text-white'>Login</Button>
     </div>
   )
 }
